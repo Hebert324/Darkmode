126413f5e472cee4509372ac6b34ae712873027a
Hebert324 <hebertrocha76@gmail.com>
2021-04-27 09:53:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const html = document.querySelector("html")
-const checkbox = document.querySelector("input[name=theme")
+const checkbox = document.querySelector("input[name=theme]")
 
-const getStyle = function(element, style) {
-    window.getComputedStyle(element).getPropertyValue(style)
-}
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
 
 const initialColors = {
-    //pegar o estilo do CSS
     bg: getStyle(html, "--bg"),
     bgPanel: getStyle(html, "--bg-panel"),
-    colorHeadings: getStyle(html, "-color-headings"),
+    colorHeadings: getStyle(html, "--color-headings"),
     colorText: getStyle(html, "--color-text"),
 }
 
 const darkMode = {
     bg: "#333333",
     bgPanel: "#434343",
-    colorHeadings: "#3664ff",
+    colorHeadings: "#3664FF",
     colorText: "#B5B5B5"
 }
 
